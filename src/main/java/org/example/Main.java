c93d51deacae84f32accb1e82e802e786073a6fc
Erwin van der Laan <erwinvanderlaan@gmail.com>
2024-10-11 10:47:26 +0200

    Streamlined 'Main' class.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,68 +24,69 @@ public class Main {
             Press '2' for rectangles.
             """);
 
-        String choice1 = askForInput("Choose an option:");
+        String choiceCircleOrRectangle = askForInput("Choose an option:");
 
-        switch (choice1) {
-            case "1":
-                System.out.println("""
+        String adjustParameters = askForInput(
+                "Do you want to adjust the parameters of the object? y / n ");
+        if (adjustParameters.equals("y"))
+            if (choiceCircleOrRectangle.equals("1")) {
+                circle.setRadius(Double.parseDouble
+                        (askForInput("Enter the radius:")));
+            }
+           if (adjustParameters.equals("2")) {
+                rectangle.setLength(Integer.parseInt
+                    (askForInput("Enter length")));
+                rectangle.setWidth(Integer.parseInt
+                    (askForInput("Enter width")));
+                }
+            else {
+               System.out.println("Invalid choice. Please try again");
+           }
+
+        if (adjustParameters.equals("n")) {
+            System.out.println("Using default values");
+        }
+
+        System.out.println("""
                         What do you want to calculate?
-                            Press '1' to calculate the diameter of the circle.
-                            Press '2' to calculate the perimeter of the circle.
-                            Press '3' to calculate the area of the circle.
+                            Press '1' to calculate the diameter ( circle only).
+                            Press '2' to calculate the perimeter.
+                            Press '3' to calculate the area.
                             Press '4' to calculate the number of poles needed.
                             Press '5' to calculate the distance between poles.
                         """);
 
-                String choice2 = askForInput("Choose an option:");
+        String choiceMenu = askForInput("Choose an option:");
 
-                switch (choice2) {
+                switch (choiceMenu) {
                     case "1":
-                        circle.getDiameter();
+                        System.out.println(circle.getDiameter() );
                         break;
                     case "2":
-                        circle.getPerimeter();
+                        if (choiceCircleOrRectangle.equals("1") )
+                            System.out.println(circle.getPerimeter() );
+                        else
+                            System.out.println(rectangle.calculatePerimeter() ) ;
                         break;
                     case "3":
-                        circle.getArea();
+                        if (choiceCircleOrRectangle.equals("1") )
+                            System.out.println(circle.getArea() );
+                        else
+                            System.out.println(rectangle.calculateArea() );
                         break;
                     case "4":
-                        circle.getNumberOfPoles();
+                        if (choiceCircleOrRectangle.equals("1"))
+                            System.out.println(circle.getNumberOfPoles() );
+                        else
+                            System.out.println(rectangle.calculateNumberOfPoles() );
                         break;
                     case "5":
-                        circle.getDistance();
+                        if (choiceCircleOrRectangle.equals("1") )
+                            System.out.println(circle.getDistance() );
+                        else
+                            System.out.println(rectangle.calculateDistance() );
                     default:
                         System.out.println("Invalid choice. Please try again");
                 }
-
-            case "2":
-                System.out.println("""
-                        What do you want to calculate?
-                            Press '1' to calculate the perimeter of the rectangle.
-                            Press '2' to calculate the area of the rectangle.
-                            Press '3' to calculate the number of poles needed.
-                            Press '4' to calculate the distance between poles.
-                        """);
-
-                String choice3 = askForInput("Choose an option:");
-
-                switch (choice3) {
-                    case "1":
-                        rectangle.calculateArea();
-                        break;
-                    case "2":
-                        rectangle.calculatePerimeter();
-                        break;
-                    case "3":
-                        rectangle.calculateNumberOfPoles();
-                        break;
-                    case "4":
-                        rectangle.calculateDistance();
-                        break;
-                    default:
-                        System.out.println("Invalid choice. Please try again");
-                }
-
-        }
     }
 }
